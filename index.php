222a8c5c1b35d8934e78f7c7c1a894517f74accc
paulyiend <paul.yiend@ffrees.co.uk>
2013-02-04 16:29:23 +0000

    Here we detangle php and html

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 //@todo detangle php and html
-$name= (isset($_REQUEST['name'])) ? $_REQUEST['name'] : 'Paul';
-echo "<br/>Hello World my name is : ".$name;
-?>
+$name= (isset($_REQUEST['name'])) ? $_REQUEST['name'] : 'Paul'; ?>
+
+<style>
+	#testing{
+		min-width: 50%;
+		min-height: 100px;
+		background-color: #efefef;
+	}
+</style>
+<div id="testing"><?php echo "<br/>Hello World my name is : ".$name;?></div>
